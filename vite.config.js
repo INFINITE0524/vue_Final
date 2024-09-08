753e0eb1d989e020bdb0cba78ee583299cc20c3c
INFINITE0524 <jasontako0524@gmail.com>
2024-09-08 23:30:15 +0800

    4

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue_Final/',
   plugins: [
     vue(),
     vueDevTools(),
